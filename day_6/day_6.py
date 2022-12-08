82738b23dbb708f701f39aaa5f20df6538ede419
Josh Purdy (ETAS-DEV/XPC-UK) <josh.purdy@etas.com>
2022-12-08 16:17:32 +0000

    changed if order for edge case that did not occur within any of the aoc input

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -14,11 +14,11 @@ def faster_search(x, data):
     last = 0
     occ_index = {'a':-1,'b':-1,'c':-1,'d':-1,'e':-1,'f':-1,'g':-1,'h':-1,'i':-1,'j':-1,'k':-1,'l':-1,'m':-1,'n':-1,'o':-1,'p':-1,'q':-1,'r':-1,'s':-1,'t':-1,'u':-1,'v':-1,'w':-1,'x':-1,'y':-1,'z':-1}
     for i in range (0, len(data)):
-        if i - last == x:
-            return i
         if (occ_index[data[i]] >= last):
             last = occ_index[data[i]] + 1
         occ_index[data[i]] = i
+        if i - last == x:
+            return i
     return -1
 
 def part_1(data):
@@ -36,6 +36,8 @@ def part_2_fast(data):
 if __name__ == "__main__":
     with open("input.txt", "r") as input_file:
         input_data = input_file.read()
+    print(part_1_fast(input_data))
+    print(part_2_fast(input_data))
     print(timeit(lambda: part_1(input_data), number=10000))
     print(timeit(lambda: part_1_fast(input_data), number=10000))
     print(timeit(lambda: part_2(input_data), number=10000))
